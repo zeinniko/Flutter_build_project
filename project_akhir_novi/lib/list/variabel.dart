1d35f4b1518f3813e1d904f3bc2d2c8a909482d1
unknown <zeinniko>
2023-01-29 21:19:37 +0700

    edit

--- project_akhir_novi/lib/list/variabel.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-var textColor = Color(0xff535353);
-var textColorLight = Color(0xffACACAC);
--- rahmat/android/app/src/main/kotlin/com/example/rahmat/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rahmat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
